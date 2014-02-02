cb5ba4a791d73a1738fa9a9442d156f879651484
alec resnick <aresnickety@gmail.com>
2014-02-02 12:16:44 -0500

    Animating motion of the square using JavaScript and a time via setInterval
    Here we set up a timer to move the square one pixel down and to the
    right every 50 milliseconds.  We also write a basic utility function to
    make it easier to grab the current position of the square.
    
    `moveSquare` adjusts the `top` and `left` attributes to simulate
    motion.  `getPosition` makes it easy for us to grab the current
    position attributes of an element and hiding some subtleties of CSS
    rendering which we’ll get to later.
    
    We’re using
    [`requestAnimationFrame`](http://creativejs.com/resources/requestanimati
    onframe/) to address some of the same underlying issues as came up in
    the original Portfolio’s scrolling jitter—

--- script.js
+++ script.js
@@ -8,3 +8,36 @@ one.id = 'one';
 two.appendChild(one);
 
 one.setAttribute('style','background-color: yellow;');
+
+function moveSquare(x, y) {
+	// Open an animation frame
+	var animationFrame = webkitRequestAnimationFrame(moveSquare);
+
+	var element = document.getElementById('two');
+	
+	var finalX = getPosition(element, 'left') + x;
+	var finalY = getPosition(element, 'top') + y;
+
+	var remainingX = finalX, remainingY = finalY;
+
+	while (remainingX > 0 && remainingY > 0) {
+		var newTop = getPosition(element, 'top') + stepSize;
+		element.style.top = newTop + 'px';
+		remainingY -= stepSize;
+
+		var newLeft = getPosition(element, 'left') + stepSize;
+		element.style.left = newLeft + 'px';
+		remainingX -= stepSize;
+	}
+
+	// Close the animation frame; we don't need the GPU any more
+	webkitCancelAnimationFrame(animationFrame);
+}
+
+function getPosition(element, attribute) {
+	return parseInt(window.getComputedStyle(element)[attribute], 10);
+}
+
+
+// Actually move the square
+moveSquare(100, 100);
